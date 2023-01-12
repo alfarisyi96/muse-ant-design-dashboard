0b392ef86e5ec309dabc9198b432c1a8c399107a
alfarisyi96 <alfarisyi96@gmail.com>
2023-01-12 14:18:50 +0700

    init

--- src/App.js
+++ src/App.js
@@ -17,19 +17,24 @@ import Rtl from "./pages/Rtl";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import LogIn from "./pages/LogIn";
 import Main from "./components/layout/Main";
 import "antd/dist/antd.css";
 import "./assets/styles/main.css";
 import "./assets/styles/responsive.css";
+import SalesOrder from "./pages/Sales/SalesOrder";
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <Route path="/login" exact component={LogIn} />
         <Route path="/sign-up" exact component={SignUp} />
         <Route path="/sign-in" exact component={SignIn} />
         <Main>
           <Route exact path="/dashboard" component={Home} />
+          <Route exact path="/sales-order" component={SalesOrder} />
+          
           <Route exact path="/tables" component={Tables} />
           <Route exact path="/billing" component={Billing} />
           <Route exact path="/rtl" component={Rtl} />
---
+++ src/pages/LogIn.js
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import {
+  Layout,
+  Button,
+  Typography,
+  Card,
+  Form,
+  Input,
+  Switch,
+  Row,
+  Col,
+} from "antd";
+import { Link } from "react-router-dom";
+const { Title } = Typography;
+const { Content } = Layout;
+
+function onChange(checked) {
+  console.log(`switch to ${checked}`);
+}
+
+export default class Login extends Component {
+  render() {
+    const onFinish = (values) => {
+      console.log("Success:", values);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log("Failed:", errorInfo);
+    };
+    return (
+      <>
+        <div className="layout-default ant-layout layout-sign-up">
+          <Content className="p-0 signin">
+            <div className="sign-up-header">
+              <div className="content">
+                <Title>Mercury ERP</Title>
+              </div>
+            </div>
+
+            <Card
+              className="card-signup header-solid h-full ant-card pt-0"
+              title={<h5>Login</h5>}
+              bordered="false"
+            >
+              <Title className="mb-15">Sign In</Title>
+              <Title className="font-regular text-muted" level={5}>
+                Enter your email and password to sign in
+              </Title>
+              <Form
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                layout="vertical"
+                className="row-col"
+              >
+                <Form.Item
+                  className="username"
+                  label="Email"
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your email!",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Email" />
+                </Form.Item>
+
+                <Form.Item
+                  className="username"
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input placeholder="Password" />
+                </Form.Item>
+
+                <Form.Item
+                  name="remember"
+                  className="aligin-center"
+                  valuePropName="checked"
+                >
+                  <Switch defaultChecked onChange={onChange} />
+                  Remember me
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{ width: "100%" }}
+                  >
+                    SIGN IN
+                  </Button>
+                </Form.Item>
+                <p className="font-semibold text-muted">
+                  Don't have an account?{" "}
+                  <Link to="/sign-up" className="text-dark font-bold">
+                    Sign Up
+                  </Link>
+                </p>
+              </Form>
+            </Card>
+          </Content>
+        </div>
+      </>
+    );
+  }
+}
